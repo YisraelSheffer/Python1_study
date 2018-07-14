837c21ad93cdc544e92d851f3824edec17f1f109
YisraelSheffer <yisrael.sheffer@gmail.com>
2018-07-14 12:41:45 +0000

    moving ex1 to python 2.7 standard

--- ex1.py
+++ ex1.py
@@ -1,15 +1,15 @@
 Year = 2018
-name = input('What is your name? ')
+name = raw_input('What is your name? ')
 while True:
-    age = input('What is your age? ')
+    age = raw_input('What is your age? ')
     try:
         age = int(age)
         if age > 100:
-            print('Invalid age')
+            print 'Invalid age'
             continue
         break
     except:
-        print('Invalid age')
+        print 'Invalid age'
         continue
 Year100 = Year + 100 - age
-print('You will turn 100 on year', Year100)
+print 'You will turn 100 on year', Year100
